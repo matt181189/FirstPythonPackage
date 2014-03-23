bc22355cfc9c5414d1783f1b36c15bc151751541
Matthew Simmonds <matt181189@btinternet.com>
2014-03-23 16:40:51 +0000

    Edited setup.py to include package_data line + changed version to 0.2.1

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 
+files = ["stuff/*"]
+
 setup (
     name='FirstPythonPackage',
-    version='0.2.0',
+    version='0.2.1',
     packages=['firstpythonpackage',],
+    package_data = {'package' : files },    
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )
