ef1e97f7e3f06e19297992ecb8fa6d8fd39e1c8a
Matthew Simmonds <matt181189@btinternet.com>
2014-03-23 16:12:07 +0000

    Changed version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup (
     name='FirstPythonPackage',
-    version='0.1.1',
+    version='0.2.0',
     packages=['firstpythonpackage',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
